1438df60829d0e6d07792827a2679613b2979f95
Neil Finnegan <13869974+finnegan28@users.noreply.github.com>
2021-09-27 10:55:53 +0100

    Project Structure fix
    changing project structure

---
+++ src/Components/SearchBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const SearchBar = ({keyword,setKeyword}) => {
+  const BarStyling = {width:"20rem",background:"#F2F1F9", border:"none", padding:"0.5rem"};
+  const BtnStyling = {width:"10rem",background:"#F2F1F9", border:"none", padding:"0.5rem"};
+  return (
+  <form>
+    <input 
+     style={BarStyling}
+     key="random1"
+     placeholder={"Search for files"}
+     value={keyword}
+    />
+    &nbsp;
+    <input
+        type='submit'
+        style={BtnStyling}
+        onSubmit={(e) => setKeyword(e.target.value)}
+    />
+ </form>
+  );
+}
+
+export default SearchBar
